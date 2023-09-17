4fa3838ba2cc91cfaf745eb6915b34276c0b5fc6
nnisarggada <gadannisarg@gmail.com>
2023-09-18 00:50:28 +0530

    v1

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,6 @@ import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://test.nnisarg.in",
+  site: "https://nnisarg.in",
   integrations: [tailwind(), robotsTxt()],
 });
--- src/utils.ts
+++ src/utils.ts
@@ -48,35 +48,97 @@ export const projects = [
   {
     title: "SWFT",
     description: "A simple temporary file sharing solution",
-    categories: ["web", "open-source", "self-host", "nextjs", "tailwindcss"],
+    categories: ["webdev", "open-source", "self-host", "nextjs", "tailwindcss"],
+    image: "SWFT.png",
     url: "https://share.nnisarg.in",
     repo: "https://github.com/nnisarggada/swft",
   },
   {
     title: "PACMAN",
     description: "An open-source contact management system",
-    categories: ["web", "open-source", "self-host", "nextjs", "tailwindcss"],
+    categories: ["webdev", "open-source", "self-host", "nextjs", "tailwindcss"],
+    image: "PACMAN.png",
     url: "https://pacman.nnisarg.in",
     repo: "https://github.com/nnisarggada/pacman",
   },
   {
     title: "Cyno5ure",
     description: "An e-commerce website built for a clothing store",
-    categories: ["web", "client-work", "shopify", "nextjs", "tailwindcss"],
+    categories: ["webdev", "client-work", "shopify", "nextjs", "tailwindcss"],
+    image: "Cyno5ure.png",
     url: "https://cyno5ure.nnisarg.in",
   },
   {
     title: "NyChess",
     description: "A simple chess game and AI coded in Python",
-    categories: ["python", "ai-ml"],
+    categories: ["python", "pygame", "ai-ml"],
     image: "NyChess.png",
     repo: "https://github.com/nnisarggada/nychess",
   },
   {
     title: "TwoDu",
     description: "A simple cross-platform To-Do Manager",
-    categories: ["web", "flutter", "firebase"],
+    categories: ["webdev", "flutter", "dart", "firebase"],
+    image: "TwoDu.png",
     url: "https://twodu.nnisarg.in",
     repo: "https://github.com/nnisarggada/twodu",
   },
+  {
+    title: "CubeRun3D",
+    description: "A simple hyper-casual mobile game",
+    categories: ["gamedev", "c-sharp", "mobile", "unity"],
+    image: "CubeRun3D.png",
+    repo: "https://github.com/nnisarggada/cuberun3d",
+    url: "https://play.google.com/store/apps/details?id=com.nnisarg.CubeRun3D&pli=1",
+  },
+  {
+    title: "OnePlayerPong",
+    description: "A unique twist on the classic pong game",
+    categories: ["gamedev", "c-sharp", "mobile", "unity"],
+    image: "OnePlayerPong.png",
+    repo: "https://github.com/nnisarggada/oneplayerpong",
+    url: "https://play.google.com/store/apps/details?id=com.nnisarg.OnePlayerPong&pli=1",
+  },
+  {
+    title: "Artistic Humans",
+    description: "Website for a theater company",
+    categories: ["webdev", "client-work", "html", "css"],
+    image: "ArtisticHumans.png",
+    repo: "https://github.com/nnisarggada/artistichumans",
+    url: "https://artistichumans.nnisarg.in",
+  },
+  {
+    title: "missionICU",
+    description: "Website for a NGO improving rural healthcare in India",
+    categories: ["webdev", "client-work", "react", "tailwindcss"],
+    image: "missionICU.png",
+    repo: "https://github.com/nnisarggada/missionicu",
+    url: "https://missionicu.nnisarg.in",
+  },
+  {
+    title: "Bhoomika",
+    description: "Website for a play about mental health",
+    categories: ["webdev", "client-work", "react", "tailwindcss"],
+    image: "Bhoomika.png",
+    repo: "https://github.com/nnisarggada/bhoomika",
+    url: "https://bhoomika.nnisarg.in",
+  },
+  {
+    title: "Wait A Millet!",
+    description: "Website for a play about Millets",
+    categories: ["webdev", "client-work", "html", "tailwindcss"],
+    image: "Millet.png",
+    repo: "https://github.com/nnisarggada/millets",
+    url: "https://millet.nnisarg.in",
+  },
 ];
+
+export const uniqueCategories: string[] = [];
+
+projects.forEach((project: any) => {
+  project.categories.forEach((category: string) => {
+    if (!uniqueCategories.includes(category)) {
+      uniqueCategories.push(category);
+    }
+  });
+});
