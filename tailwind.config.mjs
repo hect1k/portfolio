a488f54271859510a6157eed756e3c9924d966a6
nnisarggada <gadannisarg@gmail.com>
2024-11-14 15:48:34 +0530

    gpg

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,4 +1,5 @@
-/** @type {import('tailwindcss').Config} */
+import typography from "@tailwindcss/typography";
+
 export default {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
@@ -54,5 +55,5 @@ export default {
       },
     },
   },
-  plugins: [require("@tailwindcss/typography")],
+  plugins: [typography],
 };
